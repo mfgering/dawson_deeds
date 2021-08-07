72ef5cd1f69773c0cc5d259a762acd30151171a7
Mike Gering <mike@mfg-consulting-llc.com>
2021-08-07 12:16:30 -0400

    progress

--- notes.md
+++ notes.md
@@ -4,3 +4,9 @@ To create an openoffice spreadsheet from the csv file:
 ```
 "\Program Files\LibreOffice\program\soffice.exe" -convert-to ods --infilter="CSV:44,34,11,1,1/2/2/2/3/1/4/3/5/1/6/1" dawson_deeds.csv
 ```
+
+Calc Date cells:
+[python macro vs calc date](https://ask.libreoffice.org/t/python-macro-vs-calc-date-field/65187)
+
+note that you also may use Calc functions to calculate required values, using com.sun.star.sheet.FunctionAccess service
+that way, you create an instance of the service, and then use its callFunction to call e.g. DATEVALUE on the ISO date string
--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,52 @@
+import csv
 import socket  # only needed on win32-OOo3.0.0
 import uno
+import datetime
+
+def do_sheet(model):
+    try:
+        calc_fns = smgr.createInstance("com.sun.star.sheet.FunctionAccess")
+        if not model.Sheets.hasByName("dawson_deeds"):
+            idx = model.Sheets.getCount()
+            model.Sheets.insertNewByName("dawson_deeds", idx)
+        sheet = model.getSheets().getByName("dawson_deeds")
+        #thiscomponent.currentController.setActiveSheet(oNewSheet)
+        model.CurrentController.setActiveSheet(sheet)
+        with open("reports/dawson.csv") as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            row = 0
+            for row_v in csv_reader:
+                print(row)
+                for col in range(0, len(row_v)):
+                    cell = sheet.getCellByPosition(col, row)
+                    col_val = row_v[col]
+                    try:
+                        if row == 0 or col == 1:
+                            cell.String = col_val
+                        else:
+                            if col == 3:
+                                col_val = calc_fns.callFunction("DATEVALUE", [col_val])
+                            cell.Value = col_val
+                    except Exception as exc:
+                        pass
+                row += 1
+        do_autofilter(smgr, sheet)
+        print("Have sheet")
+    except Exception as exc:
+        raise exc
+        pass
+
+def do_autofilter(smgr, sheet):
+    svc = smgr.createInstance("com.sun.star.frame.DispatchHelper")
+    cell = sheet.getCellRangeByName("A1")
+    frame = model.getCurrentController().getFrame()
+    svc.executeDispatch(frame, ".uno:DataFilterAutoFilter", "", 0, [])
+
+def call_dispatch(doc, url, args=()):
+    frame = doc.getCurrentController().getFrame()
+    dispatch = create_instance('com.sun.star.frame.DispatchHelper')
+    dispatch.executeDispatch(frame, url, '', 0, args)
+    return
 
 # get the uno component context from the PyUNO runtime
 localContext = uno.getComponentContext()
@@ -17,17 +64,20 @@ desktop = smgr.createInstanceWithContext( "com.sun.star.frame.Desktop",ctx)
 # access the current writer document
 model = desktop.getCurrentComponent()
 
-# access the active sheet
-active_sheet = model.CurrentController.ActiveSheet
+do_sheet(model)
 
-# access cell C4
-cell1 = active_sheet.getCellRangeByName("C4")
+def foo(self):
+    # access the active sheet
+    active_sheet = model.CurrentController.ActiveSheet
 
-# set text inside
-cell1.String = "Hello world"
+    # access cell C4
+    cell1 = active_sheet.getCellRangeByName("C4")
 
-# other example with a value
-cell2 = active_sheet.getCellRangeByName("E6")
-cell2.Value = cell2.Value + 1
+    # set text inside
+    cell1.String = "Hello world"
+
+    # other example with a value
+    cell2 = active_sheet.getCellRangeByName("E6")
+    cell2.Value = cell2.Value + 1
 
 print("Done")
