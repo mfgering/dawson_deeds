17a2280fe39dc2ec518893f914e3906922325df9
Mike Gering <mike@mfg-consulting-llc.com>
2021-08-08 14:10:43 -0400

    cleanup

--- make-sheet.cmd
+++ make-sheet.cmd
@@ -1,5 +1,3 @@
 @echo off
 set DIR="%~dp0"
-cd reports
-"\Program Files\LibreOffice\program\soffice.exe" --headless -convert-to ods --infilter="CSV:44,34,11,1,1/2/2/2/3/1/4/3/5/1/6/1" dawson.csv
-cd %DIR%
+c:\"Program Files\LibreOffice\program\python.exe" sheet_edit.py
