c34ae4195ade7ffa9a51db459db0a6da557cd910
Mike <mike@mfg-consulting-llc.com>
2023-01-02 12:20:46 -0500

    cleanup

--- foo.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-f = open('reports/dawson.csv', 'r')
-rdr = csv.reader(f, delimiter=',')
-
-for row in rdr:
-    print(row)
--- linux_foo.py
+++
@@ -1,7 +0,0 @@
-import utils.officehelper as officehelper
-import utils.IDE_Utils
-#officehelper.bootstrap()
-
-with utils.IDE_Utils.Runner() as jesse_owens:
-    print(jesse_owens)
-print("Hi")
