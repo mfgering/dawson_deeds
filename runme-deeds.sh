51ca1bd3a651a9823b18e55528dd0192074ff2d7
mike <mike@aol.com>
2025-03-01 07:45:49 -0500

    runme

--- runme-deeds.sh
+++ runme-deeds.sh
@@ -6,8 +6,12 @@ activate() {
 
 git pull
 activate
+echo "Running dawson_deeds.py"
 python3 dawson_deeds.py
+echo "Running cypress_deeds.py"
 python3 cypress_deeds.py
+echo "Commiting changes..."
 git add reports
 git commit -m 'cron' 
 git push
+echo "Done"
